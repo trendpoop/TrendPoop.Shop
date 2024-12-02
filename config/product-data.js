39a8965a47ee3c52ce35a3c2970f78e58d1ca89b
Trend Poop <trendpoopshop@gmail.com>
2024-12-02 10:41:21 -0700

    Initial Commit

--- config/product-data.js
+++ config/product-data.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     sovrn: {
         apiKey: process.env.SOVRN_API_KEY || '753c07d9e4ac6b208ae7e1bb7569d5d6',
-        secretKey: process.env.SOVRN_SECRET_KEY || 'your_secret_key_here',
+        secretKey: process.env.SOVRN_SECRET_KEY || '73a073de1f5e017ef622d209b9bbbd77fe476cf3',
         market: process.env.SOVRN_MARKET || 'usd_en',
         rateLimit: 20
     },
